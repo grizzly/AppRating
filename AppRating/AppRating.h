d21b72b050d13661b55694b978c829487bc77024
Stefan Mayr <st.mayr@grizzly.cc>
2017-01-30 16:49:10 +0100

    Initial Commit

---
+++ AppRating/AppRating.h
@@ -0,0 +1,19 @@
+//
+//  AppRating.h
+//  AppRating
+//
+//  Created by Grizzly on 30.01.17.
+//  Copyright © 2017 Grizzly New Technologies GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppRating.
+FOUNDATION_EXPORT double AppRatingVersionNumber;
+
+//! Project version string for AppRating.
+FOUNDATION_EXPORT const unsigned char AppRatingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppRating/PublicHeader.h>
+
+
