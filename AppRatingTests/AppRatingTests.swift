32796fd410abc1b5fad2386bbcedbddf8a1baa22
Stefan Mayr <st.mayr@grizzly.cc>
2017-01-31 09:46:23 +0100

    tests setup

--- AppRatingTests/AppRatingTests.swift
+++ AppRatingTests/AppRatingTests.swift
@@ -1,19 +1,40 @@
-//
 //  AppRatingTests.swift
 //  AppRatingTests
 //
-//  Created by Grizzly on 30.01.17.
-//  Copyright © 2017 Grizzly New Technologies GmbH. All rights reserved.
+// Copyright (c) 2017 GrizzlyNT (https://github.com/grizzly/AppRating)
 //
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 
 import XCTest
 @testable import AppRating
 
 class AppRatingTests: XCTestCase {
     
+    var appID: String! = "12345678"
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        AppRating.appID = self.appID;
+    }
+    
+    func testAppIDSetProperly() {
+        XCTAssertEqual(AppRating.appID, appID, "appID should be as set")
     }
     
     override func tearDown() {
